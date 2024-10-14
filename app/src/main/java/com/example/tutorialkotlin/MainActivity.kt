6c1aaaf8b73aab6114d0c6d82567dbd8a65cd142
jonathanduran7 <jduran@genit.com.ar>
2024-10-14 20:28:24 -0300

    first commit

---
+++ app/src/main/java/com/example/tutorialkotlin/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.tutorialkotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //explicit intent
+        val explicitButton = findViewById<Button>(R.id.explicitButton)
+
+        explicitButton.setOnClickListener {
+            val intent = Intent(this, SecondActiviry::class.java)
+            startActivity(intent)
+            finish() //Optional: Finish the current activity if needed
+        }
+    }
+}
