503017a3e2864f755dc03eb92dc83da605111886
jonathanduran7 <jduran@genit.com.ar>
2024-10-14 20:46:44 -0300

    add list view

--- app/src/main/java/com/example/tutorialkotlin/ToDoActivity.kt
+++ app/src/main/java/com/example/tutorialkotlin/ToDoActivity.kt
@@ -1,6 +1,9 @@
 package com.example.tutorialkotlin
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,19 @@ class ToDoActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val listView = findViewById<ListView>(R.id.listView)
+        val listItems = arrayOf("Read a book", "Complete a project", "Learn Kotlin", "Exercise", "Go for a walk")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val selectedItem = parent.getItemAtPosition(position) as String
+            // Handle item click here
+            Toast.makeText(this, "Selected item: $selectedItem", Toast.LENGTH_SHORT).show()
+
+        }
+
+
     }
 }
